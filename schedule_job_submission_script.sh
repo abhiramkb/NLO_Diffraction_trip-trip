f081e7c9c4091310bd07319c776dbb2faca8d8cb
Abhiram Kaushik <abhiramkaushik@gmail.com>
2025-11-27 12:45:14 +0200

    Save test schedule script

--- schedule_job_submission_script.sh
+++ schedule_job_submission_script.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -euo pipefail
+#set -euo pipefail
 
 # SLURM parameters
 PARTITION="small"
@@ -8,11 +8,17 @@ ACCOUNT="lappi"
 NTASKS=1
 CPUS_PER_TASK=16
 TIME="1-23:00:00"  # HH:MM:SS format
-DRYRUN=0  # Set to 1 for dry run mode
+DRYRUN=1  # Set to 1 for dry run mode
+
+# Parameters for generating beta values corresponding to uniform spacing in MX
+M_PI=0.135
+M_MIN=$(echo "2*$M_PI" | bc -l)
+M_MIN_SQ=$(echo "$M_MIN*$M_MIN" | bc -l)
+XPMAX=0.1
 
 # Parameter lists
-X_LIST=(1e-3)
-BETA_LIST=(0.9821012055292299 0.09160527014279658 0.04804324660886743 0.032559759572223367 0.02462390979178391 0.01979840766356764 0.016554293364261168 0.014223644839570978 0.012468261261692032 0.011098554638004132 0.01)
+X_LIST=(1e-2)
+BETA_LIST=()
 Q_LIST=(2.0)
 XMAX_LIST=(40.0)
 Q0_LIST=(1.0)
@@ -41,10 +47,25 @@ fi
 for Q0 in "${Q0_LIST[@]}"; do
 	for X0 in "${X0_LIST[@]}"; do
 		for LAMBDA in "${LAMBDA_LIST[@]}"; do
-			for X in "${X_LIST[@]}"; do
-				for XMAX in "${XMAX_LIST[@]}"; do
-					for BETA in "${BETA_LIST[@]}"; do
-						for Q in "${Q_LIST[@]}"; do
+			for XMAX in "${XMAX_LIST[@]}"; do
+				for X in "${X_LIST[@]}"; do
+					for Q in "${Q_LIST[@]}"; do
+						# Convert scientific-notation X → decimal so bc can read it
+						X_DEC=$(printf "%.15f" "$X")
+						QSQ=$(echo "$Q*$Q" | bc -l)
+
+        					# Compute M_max^2
+						MMAX_SQ=$(echo "$QSQ * ($XPMAX/$X_DEC - 1)" | bc -l)
+
+						BETA_LIST=()
+		
+						for i in $(seq 0 0.1 0.2); do
+					        	Msq=$(echo "$M_MIN_SQ + ($MMAX_SQ - $M_MIN_SQ)*$i" | bc -l)
+					        	beta=$(echo "$QSQ / ($QSQ + $Msq)" | bc -l)
+					        	BETA_LIST+=("$beta")    # append to bash array
+					        done
+        					echo
+        					for BETA in "${BETA_LIST[@]}"; do						
 							echo "Submitting job in experiment $EXPERIMENT_NAME with xmax = $XMAX, x=$X, Q=$Q and BETA=$BETA"
 							CMD="sbatch --parsable --partition=$PARTITION --account=$ACCOUNT --ntasks=$NTASKS --cpus-per-task=$CPUS_PER_TASK --mem=$MEMORY --time=$TIME --job-name=\"${EXPERIMENT_NAME}\" --output=\"${SLRM_OUTPUT_DIR}/${EXPERIMENT_NAME}_%j.out\" --error=\"${SLRM_OUTPUT_DIR}/${EXPERIMENT_NAME}_%j.err\" ${JOB_SCRIPT} ${EXEC} $Q $BETA $X $XMAX $NEVAL $Q0 $X0 $LAMBDA"
 					
