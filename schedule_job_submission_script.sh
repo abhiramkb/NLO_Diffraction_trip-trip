caaa224c9e7dffc328bc3d22d1f1601cb3c35255
Abhiram Kaushik <abhiramkaushik@gmail.com>
2026-04-02 21:45:36 +0300

    Adding HERA kinematics points list and modified scheduler

--- schedule_job_submission_script.sh
+++ schedule_job_submission_script.sh
@@ -7,8 +7,8 @@ PARTITION="small"
 GRES="gpu:v100:1"
 ACCOUNT="lappi"
 NTASKS=1
-CPUS_PER_TASK=16
-TIME="1-23:00:00"  # HH:MM:SS format
+CPUS_PER_TASK=8
+TIME="00:12:00"  # HH:MM:SS format
 DRYRUN=1  # Set to 1 for dry run mode
 
 # Parameters for generating beta values corresponding to uniform spacing in MX
@@ -37,6 +37,7 @@ MEMORY="4G"
 LANGUAGE="python"
 EXEC="photon_T_grid.py"
 DIPOLE="bk_kcbk_pd_map.dat"
+PARAM_FILE="HERA_kinematic_points.txt" # File with Q, beta and xpom values
 JOB_SCRIPT="submit_job.sh" #The job script takes code name, parameters etc as arguments
 
 # File to store submitted job IDs (for watcher script)
@@ -47,25 +48,29 @@ if [ "$DRYRUN" -eq 1 ]; then
 	echo "DRY RUN:"
 	echo ""
 fi
+if [ ! -f "$PARAM_FILE" ]; then
+    echo "Error: $PARAM_FILE not found!"
+    exit 1
+fi
 
-# Loop over parameter combinations
+# Loop over each line in the parameter file
 for XMAX in "${XMAX_LIST[@]}"; do
-	for Q in "${Q_LIST[@]}"; do
-		for XPOM in "${XPOM_LIST[@]}"; do
-			for BETA in "${BETA_LIST[@]}"; do
-    			echo "Submitting job in experiment $EXPERIMENT_NAME with x_pom=$XPOM, Q=$Q and BETA=$BETA"
-				CMD="sbatch --parsable --partition=$PARTITION --account=$ACCOUNT --ntasks=$NTASKS --cpus-per-task=$CPUS_PER_TASK --mem=$MEMORY --gres=$GRES --time=$TIME --job-name=\"${EXPERIMENT_NAME}\" --output=\"${SLRM_OUTPUT_DIR}/${EXPERIMENT_NAME}_%j.out\" --error=\"${SLRM_OUTPUT_DIR}/${EXPERIMENT_NAME}_%j.err\" ${JOB_SCRIPT} ${LANGUAGE} ${EXEC} $Q $BETA $XPOM $XMAX $NEVAL $DIPOLE"
-						
-				if [ "$DRYRUN" -eq 1 ]; then
-					echo "$CMD"
-				else
-					jid=$(eval "$CMD")  # --parsable ensures only job ID is returned
-					echo "Submitted job $jid"
-					echo "$jid" >> "$JOBID_FILE"
-				fi
-			done
-		done
-	done
+    while read -r Q_VAL BETA XPOM; do
+        
+        Q=$Q_VAL 
+
+        echo "Submitting: Q=$Q_VAL, BETA=$BETA, XPOM=$XPOM"
+
+        CMD="sbatch --parsable --partition=$PARTITION --account=$ACCOUNT --ntasks=$NTASKS --cpus-per-task=$CPUS_PER_TASK --mem=$MEMORY --gres=$GRES --time=$TIME --job-name=\"${EXPERIMENT_NAME}\" --output=\"${SLRM_OUTPUT_DIR}/${EXPERIMENT_NAME}_%j.out\" --error=\"${SLRM_OUTPUT_DIR}/${EXPERIMENT_NAME}_%j.err\" ${JOB_SCRIPT} ${LANGUAGE} ${EXEC} $Q $BETA $XPOM $XMAX $NEVAL $DIPOLE"
+
+        if [ "$DRYRUN" -eq 1 ]; then
+            echo "$CMD"
+        else
+            jid=$(eval "$CMD")
+            echo "$jid" >> "$JOBID_FILE"
+        fi
+
+    done < "$PARAM_FILE"
 done
 echo "All jobs submitted. Job IDs written to $JOBID_FILE"
 echo "Run ./wait_and_collect.sh to wait for completion and run the collector."
